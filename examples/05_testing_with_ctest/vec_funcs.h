d9ca2ab00a4484df16193cc1339c18d870ffd70c
James Percival <j.percival@imperial.ac.uk>
2023-05-09 12:04:56 +0100

    missing file.

---
+++ examples/05_testing_with_ctest/vec_funcs.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <vector>
+
+// sum the elements of a vector by hand
+int my_sum(std::vector<int>&);
+// multiply the elements of a vector by hand
+int my_prod(std::vector<int>&);
